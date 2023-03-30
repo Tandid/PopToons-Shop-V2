7a95b57379d98260c19868bcebfe2e687de160dc
Tandid Alam <tandid.alam@gmail.com>
2023-03-30 01:41:35 -0400

    Added Products to Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
 import Layout from "../components/Layout";
+import products from "../utils/data";
+import ProductItem from "../components/ProductItem";
 
 const Home: React.FC = (): React.ReactElement => {
-  return <Layout title="Home">Home Page</Layout>;
+  return (
+    <Layout title="Home">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols3 lg:grid-cols-4">
+        {products.map((product) => (
+          <ProductItem product={product} key={product.slug}></ProductItem>
+        ))}
+      </div>
+    </Layout>
+  );
 };
 
 export default Home;
