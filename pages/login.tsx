c3c577b7944d4f8015bbc773a916c9b0e19f96d8
Tandid Alam <tandid.alam@gmail.com>
2023-03-30 19:54:03 -0400

    Added some Login Logic

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,20 +1,69 @@
 import React from "react";
 import Layout from "../components/Layout";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
 
 const LoginScreen: React.FC = (): React.ReactElement => {
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z0-9-.]+$/i;
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+  const submitHandler = ({ email, password }) => {
+    console.log(email, password);
+  };
   return (
     <Layout title="Login">
-      <form className="mx-auto max-w-screen-md">
+      <form
+        className="mx-auto max-w-screen-md"
+        onSubmit={handleSubmit(submitHandler)}
+      >
         <h1 className="mb-4 text-xl">Login</h1>
+
+        {/* Email */}
+
         <div className="mb-4">
           <label htmlFor="email">Email</label>
-          <input type="email" className="w-full" id="email" autoFocus />
+          <input
+            type="email"
+            {...register("email", {
+              required: "Please Enter Email",
+              pattern: {
+                value: { emailPattern },
+                message: "Invalid email address",
+              },
+            })}
+            className="w-full"
+            id="email"
+            autoFocus
+          />
+          {errors.email && (
+            <div className="text-red-500">{errors.email.message}</div>
+          )}
         </div>
+
+        {/* Password */}
+
         <div className="mb-4">
           <label htmlFor="password">Password</label>
-          <input type="password" className="w-full" id="password" autoFocus />
+          <input
+            type="password"
+            className="w-full"
+            {...register("password", {
+              required: "Please Enter Password",
+              minLength: {
+                value: 6,
+                message: "Password must be at least 6 characters",
+              },
+            })}
+            id="password"
+            autoFocus
+          />
         </div>
+
+        {/* Login Button */}
+
         <div className="mb-4">
           <button className="primary-button">Login</button>
         </div>
