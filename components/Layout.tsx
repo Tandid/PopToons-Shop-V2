33fa71971f7ae4f529293babd0276d71a0ac43eb
Tandid Alam <tandid.alam@gmail.com>
2023-03-30 00:21:21 -0400

    Added Layout, Nav, Home, Footer

---
+++ components/Layout.tsx
@@ -0,0 +1,37 @@
+import Head from "next/head";
+import Link from "next/link";
+import React from "react";
+
+export default function Layout({ title, children }) {
+  return (
+    <>
+      <Head>
+        <title>{title ? `${title} - Poptoons` : "Poptoons"}</title>
+        <meta name="description" content="ECommerce Website" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex min-h-screen flex-col justify-between">
+        <header>
+          <nav className="flex h-12 justify-between shadow-md items-center px-4">
+            <Link href="/">
+              <h1 className="text-lg font-bold">Poptoons</h1>
+            </Link>
+            <div>
+              <Link className="p-2" href="/cart">
+                Cart
+              </Link>
+              <Link className="p-2" href="/login">
+                Login
+              </Link>
+            </div>
+          </nav>
+        </header>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex justify-center h-10 items-center shadow-inner">
+          Copyright @ 2023 Poptoons Shop
+        </footer>
+      </div>
+    </>
+  );
+}
